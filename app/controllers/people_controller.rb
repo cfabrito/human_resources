3efdbc4cc8dacc4e6009cc490d5e3d44a2a54967
Carlos Brito <cfabrito@gmail.com>
2014-08-27 21:54:34 +0100

    Add methods to controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,28 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
+  end
+  
+  def new
+    @article = Person.new
   end
   
   def create
-    render text: params[:person].inspect
+    @person = Person.new(person_params)
+ 
+    if @person.save
+      redirect_to @person
+    else
+      render 'new'
+    end
   end
+  
+  def show
+    @person = Person.find(params[:id])
+  end
+  
+  private
+    def person_params
+      params.require(:person).permit(:first_name, :last_name, :phone_number, :email, :birth_date, :linkedin_url)
+    end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,14 @@
 class Person < ActiveRecord::Base
+  validates :first_name,  presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
+  validates :phone_number, presence: true
+  validates :email, presence: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ },
+                    uniqueness: { case_sensitive: false }
+  validates :birth_date, presence: true
+  
+  # validates :linkedin_url, presence: true
+
+  def name
+    return "#{first_name} #{last_name}"
+  end
 end
