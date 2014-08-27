d5b4fdd921f39dcaa56ad739569cc0eb96529ef8
Carlos Brito <cfabrito@gmail.com>
2014-08-27 16:18:33 +0100

    Clean project with --skip-test-unit

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-Rails.application.load_tasks
+HumanResources::Application.load_tasks
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HumanResources::Application.config.secret_key_base = 'aeaaf1d121225868fa42d4ec173bdf006ea96005480b819694e6ee1b81cf80df1b34803bb12aba0fc80ed52489793943b33fd9256f90b473abee46a49a9031ef'
