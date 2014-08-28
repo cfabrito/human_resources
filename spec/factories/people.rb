878155648c26f15ed6167158ae7999ab220df672
Carlos Brito <cfabrito@gmail.com>
2014-08-28 12:53:43 +0100

    Add basic tests for Person model

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,12 +1,14 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+require 'faker'
+
 FactoryGirl.define do
   factory :person do
-    first_name "MyString"
-    last_name "MyString"
-    phone_number "MyString"
-    email "MyString"
-    birth_date "2014-08-27"
-    linkedin_url "MyString"
+    first_name    { Faker::Name.first_name }
+    last_name     { Faker::Name.last_name }
+    phone_number  { Faker::Number.number(9) }
+    email         { Faker::Internet.email }
+    birth_date    { Faker::Date.birthday(min_age = 18, max_age = 65) }
+    linkedin_url  { Faker::Internet.url('linkedin.com') }
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    FactoryGirl.create(:person).should be_valid
+  end
+  it "is invalid without a firstname" do
+    FactoryGirl.build(:person, first_name: nil).should_not be_valid
+  end
+  it "is invalid without a lastname" do
+    FactoryGirl.build(:person, last_name: nil).should_not be_valid
+  end
+  it "is invalid without a phone number" do
+    FactoryGirl.build(:person, phone_number: nil).should_not be_valid
+  end
+  it "is invalid without an email" do
+    FactoryGirl.build(:person, email: nil).should_not be_valid
+  end
+  it "is invalid without a birthdate" do
+    FactoryGirl.build(:person, birth_date: nil).should_not be_valid
+  end
+  it "returns a person's full name as a string" do
+    person = FactoryGirl.build(:person, first_name: "John", last_name: "Doe")
+    person.name.should == "John Doe"
+  end
 end
